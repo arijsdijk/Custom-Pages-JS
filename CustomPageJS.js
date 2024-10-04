cf5d9410e432495c5770959318cdeb67ad364e82
Arjan Rijsdijk <arjan.rijsdijk@outlook.com>
2024-10-04 08:42:46 +0200

    Add files via upload

---
+++ CustomPageJS.js
@@ -0,0 +1,22 @@
+
+function openCustomPage(pageContext, paramPage, paramTitle, paramPosition, paramWidth, paramHeight){
+
+    Xrm.Navigation.navigateTo(
+      { 
+      pageType: "custom",  
+      name: paramPage, // "cr0d0_custommodal_d31f6",  
+      entityName: pageContext.data.entity.getEntityName(),  
+      recordId: pageContext.data.entity.getId()
+      }, 
+    
+      {
+         target:2, 
+         position: paramPosition,
+         width: {value:paramWidth, unit:"%"}, 
+         height: {value: paramHeight, unit: "%"},
+         title: paramTitle
+      }
+
+   ).then(console.log).catch(console.error);
+   
+}
