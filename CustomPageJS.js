9b77a5a5899b03241ba3ee08ff76988c23a790ff
Arjan Rijsdijk <arjan.rijsdijk@outlook.com>
2024-10-12 19:59:37 +0200

    Update

--- CustomPageJS.js
+++ CustomPageJS.js
@@ -1,8 +1,7 @@
 
 function openCustomPage(pageContext, paramPage, paramTitle, paramPosition, paramWidth, paramHeight){
 
-    Xrm.Navigation.navigateTo(
-      { 
+   Xrm.Navigation.navigateTo({ 
       pageType: "custom",  
       name: paramPage, // "cr0d0_custommodal_d31f6",  
       entityName: pageContext.data.entity.getEntityName(),  
@@ -10,11 +9,11 @@ function openCustomPage(pageContext, paramPage, paramTitle, paramPosition, param
       }, 
     
       {
-         target:2, 
-         position: paramPosition,
-         width: {value:paramWidth, unit:"%"}, 
-         height: {value: paramHeight, unit: "%"},
-         title: paramTitle
+      target:2, 
+      position: paramPosition,
+      width: {value:paramWidth, unit:"%"}, 
+      height: {value: paramHeight, unit: "%"},
+      title: paramTitle
       }
 
    ).then(console.log).catch(console.error);
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Custom Pages JS
 A JS file for adding custom pages in a model-driven app.
+
+## Parameter 1
+
+## Parameter 2
