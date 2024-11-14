face2f8011bacc613bceacb7f864e3da9a39d4a7
Arjan Rijsdijk <a.rijsdijk@me.com>
2024-11-14 21:04:18 +0100

    Commit changes

--- CustomPageJS.js
+++ CustomPageJS.js
@@ -1,15 +1,12 @@
 
 function openCustomPage(pageContext, paramPage, paramTitle, paramPosition, paramWidth, paramHeight){
    
-
    Xrm.Navigation.navigateTo({ 
       pageType: "custom",  
       name: paramPage, // "cr0d0_custommodal_d31f6",  
       entityName: pageContext.data.entity.getEntityName(),  
       recordId: pageContext.data.entity.getId()
-      },
-
-    
+      },   
       {
       target:2, 
       position: paramPosition,
@@ -18,8 +15,6 @@ function openCustomPage(pageContext, paramPage, paramTitle, paramPosition, param
       title: paramTitle
       }
 
-
    ).then(console.log).catch(console.error);
-
    
 }
